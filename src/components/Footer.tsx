6d8da3af9850cb8a21f83ff1238643bab44a0594
Abdullahi254 <abdush504@gmail.com>
2023-12-07 22:52:21 +0300

    created a header and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import logo from "../../public/Sheng.png"
+
+type Props = {}
+
+const Footer = (props: Props) => {
+
+    return (
+        <nav className={`w-full h-12 bg-gradient-to-r from-[#ffffff] to-[#dbdcd6] border-t-[1px] border-gray-200 px-8 py-2 z-[100]`}>
+            <div className='lg:flex items-center h-full justify-between mx-auto max-w-7xl'>
+
+                <div className='flex w-full justify-evenly space-x-10 h-full items-center'>
+                    <div className='cursor-pointer'>
+                        <Image src={logo} width={40} height={40} alt='sheng icon' priority />
+                    </div>
+                    <div className='space-x-8'>
+                        <Link href="/">
+                            <span className={`text-gray-500 text-sm hover:text-gray-900 hover:underline`}>
+                                Home
+                            </span>
+                        </Link>
+                        <Link href="/dictionary">
+                            <span className={`text-gray-500 text-sm hover:text-gray-900 hover:underline`}>
+                                Dictionary
+                            </span>
+                        </Link>
+                        <Link href="/contact">
+                            <span className={`text-gray-500 text-sm hover:text-gray-900 hover:underline`}>
+                                Contact
+                            </span>
+                        </Link>
+                        <Link href="https://github.com/Abdullahi254" target='_blank'>
+                            <span className={`text-gray-500 text-sm hover:text-gray-900 hover:underline`}>
+                                github
+                            </span>
+                        </Link>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import logo from "../../public/Sheng.png"
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+
+    const [shadow, setShadow] = useState<boolean>(false)
+
+
+    useEffect(() => {
+        const handleShadow = () => {
+            if (window.scrollY >= 90) {
+                setShadow(true)
+            } else {
+                setShadow(false)
+            }
+        }
+        window.addEventListener('scroll', handleShadow)
+    }, [])
+    return (
+        <nav className={shadow ? `shadow-xl bg-gradient-to-r from-[#dbdcd6] to-[#ffffff] fixed top-0 w-full h-12 border-b-[1px] border-gray-200 px-8 z-[100]` : ` bg-gray-200 fixed top-0 w-full h-12 border-b-[1px] border-gray-200 px-8 z-[100]`}>
+            <div className='lg:flex items-center h-full justify-between mx-auto max-w-7xl'>
+
+                <div className='flex space-x-10 h-full items-center'>
+                    <div className='cursor-pointer'>
+                        <Image src={logo} width={40} height={40} alt='sheng icon' priority />
+                    </div>
+                    <div className='space-x-8'>
+                        <Link href="/">
+                            <span className={`text-gray-500 text-sm hover:text-gray-900 hover:underline`}>
+                                Home
+                            </span>
+                        </Link>
+                        <Link href="/dictionary">
+                            <span className={`text-gray-500 text-sm hover:text-gray-900 hover:underline`}>
+                                Dictionary
+                            </span>
+                        </Link>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
