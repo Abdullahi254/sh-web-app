67809b55dbac45da4afa778c4a6d0aa6b8424568
Abdullahi254 <abdush504@gmail.com>
2023-12-07 22:52:47 +0300

    created a landing page

---
+++ src/app/dictionary/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div className='h-screen flex flex-col justify-center items-center'>
+        <h1>COMING SOON!</h1>
+    </div>
+  )
+}
+
+export default page
