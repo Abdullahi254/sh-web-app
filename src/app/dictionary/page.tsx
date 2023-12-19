a15d496fb000f666b5a1f7df07332b797a8e6acd
Abdullahi254 <abdush504@gmail.com>
2023-12-19 18:03:04 +0300

    changed the components structures to client component having server component children

--- src/app/dictionary/page.tsx
+++ src/app/dictionary/page.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react'
 import DropDown from '@/components/DropDown'
+import WordsList from '@/components/WordsList'
 export type Word = {
     id: number
     meaning: string
@@ -10,33 +11,22 @@ export type Word = {
 }
 type Props = {}
 
-async function getWordsByLetter(letter: string): Promise<Word[]> {
-    try {
-        const res = await fetch(`https://sheng-api.onrender.com/api/word?letter=${letter}`)
-        return res.json()
-
-    } catch (er) {
-        throw new Error('Failed to fetch data')
-    }
-}
 
 
-const page = async (props: Props) => {
-    const fetchWords = async (letter: string) => {
-        const data = await getWordsByLetter(letter)
-        return data
-    }
+
+const page = (props: Props) => {
     return (
         <main className="flex flex-col space-y-4 min-h-screen items-center py-20 px-6 max-w-4xl mx-auto">
             {
                 [...Array(26)].map((_, index) =>
-                    <Suspense key={index} fallback={<div>Loading...</div>}>
-                        <DropDown
-                            triggerFetch={fetchWords(String.fromCharCode("A".charCodeAt(0) + index))}
-                        >
-                            {String.fromCharCode("A".charCodeAt(0) + index)}
-                        </DropDown>
-                    </Suspense>
+                    <DropDown
+                        key={String.fromCharCode("A".charCodeAt(0) + index)}
+                        letter={String.fromCharCode("A".charCodeAt(0) + index)}
+                    >
+                        < Suspense fallback={< div > Loading...</div >}>
+                            <WordsList letter={String.fromCharCode("A".charCodeAt(0) + index)} />
+                        </Suspense>
+                    </DropDown>
                 )
             }
         </main>
--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,36 +1,33 @@
-'use client';
-import Link from 'next/link';
+"use client";
 import React, { ReactNode, useState } from 'react'
 import { IoIosArrowDown } from "react-icons/io";
-import { Word } from '@/app/dictionary/page';
+
+
 type Props = {
-    children: ReactNode,
-    triggerFetch: Promise<Word[]>
+    letter: string
+    children: ReactNode
 }
 
-const DropDown = ({ children, triggerFetch }: Props) => {
+const DropDown = ({ letter, children }: Props) => {
     const [show, setShow] = useState<boolean>(false)
-    const [words, setWords] = useState<Word[]>()
-    const openDropDown = () => {
+    const openDiv = async () => {
         setShow(prev => !prev)
-        if (show === true) {
-            triggerFetch.then(res => {
-                setWords(res as Word[])
-            })
-        }
     }
     return (
         <div className='flex flex-col items-center w-full p-4 shadow-lg hover:shadow-xl rounded-lg bg-white relative'>
-            <h1 className='font-serif text-sm md:text-base font-bold'>{children}</h1>
-            <span className='absolute right-0 top-5 pe-4 text-base md:text-[20px] cursor-pointer' onClick={openDropDown}>
+            <h1 className='font-serif text-sm md:text-base font-bold'>{letter}</h1>
+            <span className='absolute right-0 top-5 pe-4 text-base md:text-[20px] cursor-pointer' onClick={openDiv}>
                 <IoIosArrowDown className={show && "rotate-180"} />
             </span>
-            <div className={show ? 'min-h-[200px] flex justify-center space-x-4 flex-wrap py-4 border-t-2 border-b-2' : 'hidden'}>
-                {words?.map((val) => <Link key={val.id} href={`/search/${val.word}`} className='h-fit'>
-                    <span className='font-serif hover:bg-sky-100 hover:underline'>{val.word}</span>
-                </Link>)}
-            </div>
+            {
+                show &&
+                <div className='min-h-[200px] flex justify-center space-x-4 flex-wrap py-4 border-t-2 border-b-2'>
+                    {children}
+                </div>
+            }
+
         </div>
+
     )
 }
 
